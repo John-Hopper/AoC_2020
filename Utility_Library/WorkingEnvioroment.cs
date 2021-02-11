069a5ec0476aed1af8e43ee1ff5c9897f3dde9ed
John Hopper <JohnHopper.Dev@outlook.com>
2021-02-11 12:46:28 +0000

    Initial Day 1 solution and addition of Utility_Library

---
+++ Utility_Library/WorkingEnvioroment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Utility_Library
+{
+    public class WorkingEnvioroment
+    {
+        public string OsVersion { get; set; }
+        public string CurrentDirectory { get; set; }
+        public string MachineName { get; set; }
+        public string UserDomainName { get; set; }
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Returns various properties for environment the application is running in.
+        /// </summary>
+        /// <returns>WorkingEnvioroment class</returns>
+        public WorkingEnvioroment EnviromentInfo()
+        {
+            WorkingEnvioroment dataReturn = new WorkingEnvioroment();
+
+            dataReturn.OsVersion = Convert.ToString(Environment.OSVersion);
+            dataReturn.CurrentDirectory = Convert.ToString(Environment.CurrentDirectory);
+            dataReturn.MachineName = Convert.ToString(Environment.MachineName);
+            dataReturn.UserDomainName = Convert.ToString(Environment.UserDomainName);
+            dataReturn.UserName = Convert.ToString(Environment.UserName);
+
+            return dataReturn;
+        }
+    }
+}
